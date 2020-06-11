4a52d6e97075daf9a4dfea023fd8806ee08d8e7c
MatiusP <45049910+MatiusP@users.noreply.github.com>
2020-06-11 12:55:07 +0300

    Topic09 - String_basic-Tasks

--- src/by/epamtc/protsko/topic09/stringbasic/Task_01.java
+++ src/by/epamtc/protsko/topic09/stringbasic/Task_01.java
@@ -1,4 +1,41 @@
 package by.epamtc.protsko.topic09.stringbasic;
 
+//Четные и нечетные символы разделить по разным строкам
+
 public class Task_01 {
+    private static StringBuilder evenLineCharacter = new StringBuilder();
+    private static StringBuilder oddLineCharacter = new StringBuilder();
+
+    private static void divisionEvenAndOddCharacter(String primaryString) {
+        for (int i = 0; i < primaryString.length(); i++) {
+            if ((i + 1) % 2 == 0) {
+                evenLineCharacter.append(primaryString.charAt(i));
+            } else {
+                oddLineCharacter.append(primaryString.charAt(i));
+            }
+        }
+    }
+
+    public static String getEvenCharacterString(String primaryString) {
+        if (evenLineCharacter.length() == 0) {
+            divisionEvenAndOddCharacter(primaryString);
+        }
+        return evenLineCharacter.toString();
+    }
+
+    public static String getOddCharacterString(String primaryString) {
+        if (oddLineCharacter.length() == 0) {
+            divisionEvenAndOddCharacter(primaryString);
+        }
+        return oddLineCharacter.toString();
+    }
+
+
+    //----- check result -----
+    public static void main(String[] args) {
+        String line = "I like java";
+
+        System.out.println(getOddCharacterString(line));
+        System.out.println(getEvenCharacterString(line));
+    }
 }
---
+++ src/by/epamtc/protsko/topic09/stringbasic/Task_02.java
@@ -0,0 +1,43 @@
+package by.epamtc.protsko.topic09.stringbasic;
+
+//Вычислить процентное соотношение строчных и прописных букв
+
+public class Task_02 {
+    static int countUppercaseLetters = 0;
+    static int countLowercaseLetters = 0;
+
+    private static void countUppercaseAndLowercaseLetter(String stringLine) {
+        String onlyLettersLine = stringLine.replaceAll("[\\W\\d_]", "");
+        char[] lettersArray = onlyLettersLine.toCharArray();
+
+        for (char symbol : lettersArray) {
+            if (symbol == Character.toUpperCase(symbol)) {
+                countUppercaseLetters++;
+            } else {
+                countLowercaseLetters++;
+            }
+        }
+    }
+
+    public static double getPercentageRatioUppercaseAndLowercaseLetter(String stringLine) {
+        countUppercaseAndLowercaseLetter(stringLine);
+        if (countUppercaseLetters == 0) {
+            System.out.println("In string line only lowercase letters.");
+            return 0;
+        } else if (countLowercaseLetters == 0) {
+            System.out.println("In string line only uppercase letters");
+            return 100;
+        } else {
+            return ((double) countUppercaseLetters / countLowercaseLetters) * 100;
+        }
+    }
+
+
+    //----- check result -----
+    public static void main(String[] args) {
+        String line1 = "iiii Java hello  ";
+        System.out.printf("Percentage ratio Uppercase and Lowercase letters = %5.2f"
+                , getPercentageRatioUppercaseAndLowercaseLetter(line1));
+    }
+
+}
